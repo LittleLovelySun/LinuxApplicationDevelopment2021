eba944952ff3d584e99400228fbfc85deab65ee4
Svetlana Kudinova <sd_kudinova@ispras.ru>
2021-12-12 15:45:56 +0300

    Add russian (in help)

--- 11_Documenting/guess_game.c
+++ 11_Documenting/guess_game.c
@@ -11,38 +11,36 @@
 const char roman[101][10] = {"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
 
 int main(int argc, char *argv[]) {
-	const char* HELP = _("Usage ./guess_game [-r|--help]\n    -r      use roman\n    --help  show this help message and exit\n");
+	char *dir;
 	
-	if (argc > 1) {
+	dir = dirname(realpath(argv[0], NULL));
+	setlocale (LC_ALL, "");
+	bindtextdomain ("guess_game", LOCALE_PATH);
+	textdomain ("guess_game");
+	
+	if (argc > 2) {
 		printf(_("Incorrect argument count\n"));
-		printf("%s", HELP);
+		printf(_("Usage ./guess_game [-r|--help]\n    -r      use roman\n    --help  show this help message and exit\n"));
 		return -1;
 	}
 
 	int in_roman = 0;
 
-	if (argc == 1) {
-		if (argv[0] == "--help") {
-			printf("%s", HELP);
+	if (argc == 2) {
+		if (!strcmp(argv[1], "--help")) {
+			printf(_("Usage ./guess_game [-r|--help]\n    -r      use roman\n    --help  show this help message and exit\n"));
 			return 0;
 		}
-		else if (argv[0] == "-r")
+		else if (!strcmp(argv[1], "-r"))
 			in_roman = 1;
 		else {
-			printf(_("Incorrect arguments count\n"));
-			printf("%s", HELP);
+			printf(_("Incorrect argument\n"));
+			printf(_("Usage ./guess_game [-r|--help]\n    -r      use roman\n    --help  show this help message and exit\n"));
 			return -1;
 		}
 	}
 
 	int i;
-	char *dir;
-
-	dir = dirname(realpath(argv[0], NULL));
-	setlocale (LC_ALL, "");
-	bindtextdomain ("guess_game", LOCALE_PATH);
-	textdomain ("guess_game");
-
 	const char *yes = _("yes");
 	const char *no = _("no");
 
